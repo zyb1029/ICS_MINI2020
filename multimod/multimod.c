1c390fc7d635a44600f4ad22e364c50ccce298da
tracer-nju <tracer@nju.edu.cn>
2020-09-29 03:57:35 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 31a5163ba94f4579abc9eaff347b751b     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  03:57:36 up  3:47,  3 users,  load average: 0.13, 0.14, 0.13

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,9 +1,24 @@
 #include<stdint.h>
 
+uint64_t quick_mod(uint32_t a, uint32_t b) {
+	while(1) {
+		if (b > a) return a;
+		if ((b << 1) > a || b >= (1<<63) ) {
+			break;
+		}
+		b = b << 1;
+	}
+	while(b) {
+		if (a >= b) a -= b;	
+		b = b >> 1;
+	}
+	return a;
+}
+
 uint64_t ksc(uint64_t a, uint64_t b, uint64_t m) {
 	uint64_t ans = 0;
 	while(b) {
-		if(b & 1) ans = ans + a;
+		if (b & 1) ans = ans + a;
 		if (ans >= m) ans = ans - m;
 		a = a + a;
 		if (a >= m) a = a - m;
@@ -12,6 +27,7 @@ uint64_t ksc(uint64_t a, uint64_t b, uint64_t m) {
 	return ans;
 }
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
+  a = quick_mod(a, m);
   while (a >= m) a = a - m;
   return ksc(a, b, m); // buggy
 }
