a817591a823471168e62b0ac9f69b0eb86f62d04
tracer-nju <tracer@nju.edu.cn>
2020-09-29 02:26:42 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 31a5163ba94f4579abc9eaff347b751b     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  02:26:42 up  2:18,  1 user,  load average: 0.30, 0.17, 0.11

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,5 +1,16 @@
 #include <stdint.h>
 
+uint64_t ksc(uint64_t a, uint64_t b, uint64_t m) {
+	uint64_t ans = 0;
+	while(b) {
+		if(b & 1) ans = ans + a;
+		if (ans > m) ans = ans - m;
+		a = a + a;
+		if (a > m) a = a - m;
+		b >> = 1;
+	}
+	return ans;
+}
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  return (a * b) % m; // buggy
+  return ksc(a, b, m); // buggy
 }
