896a1ccc3818e543978fe8421e75bd06f182adac
tracer-nju <tracer@nju.edu.cn>
2020-09-29 04:44:54 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 31a5163ba94f4579abc9eaff347b751b     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  04:44:54 up  4:34,  4 users,  load average: 0.38, 0.25, 0.20

--- multimod/main.c
+++ multimod/main.c
@@ -1,16 +1,19 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include <assert.h>
+#include <stdlib.h>
 uint64_t multimod(uint64_t, uint64_t, uint64_t);
 
 void test(uint64_t a, uint64_t b, uint64_t m) {
   #define U64 "%" PRIu64
   printf(U64 " * " U64 " mod " U64 " = " U64 "\n", a, b, m, multimod(a, b, m));
+  assert(multimod(a,b,m) == a*b%m);
 }
 
 int main() {
-	test(4,6,20);
   test(123, 456, 789);
   test(123, 456, -1ULL);
   test(-2ULL, -2ULL, -1ULL); // should be 1
+  for (int i = 1; i <= 10000; i++)test(rand(),rand(),rand());
 }
