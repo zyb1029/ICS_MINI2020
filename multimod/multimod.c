6291e9ff8b2545378eda23310393c53eb6e69863
tracer-nju <tracer@nju.edu.cn>
2020-09-29 04:14:24 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 31a5163ba94f4579abc9eaff347b751b     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  04:14:24 up  4:04,  3 users,  load average: 0.34, 0.25, 0.16

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -3,7 +3,7 @@
 uint64_t quick_mod(uint64_t a, uint64_t b) {
 	while(1) {
 		if (b > a) return a;
-		if ((b << 1) > a || b >= ((uint64_t)1<<63) ) {
+		if ((b << 1) > a || b >= (1ULL<<63) ) {
 			break;
 		}
 		b = b << 1;
@@ -14,19 +14,22 @@ uint64_t quick_mod(uint64_t a, uint64_t b) {
 	}
 	return a;
 }
-
-uint64_t ksc(uint64_t a, uint64_t b, uint64_t m) {
+uint64_t add(uint64_t a, uint64_t b, uint64_t m, uint64_t p) {
+	if (a + b < a) return quick_mod(a + b, m) + p;
+	else return quick_mod(a + b, m);
+}
+uint64_t ksc(uint64_t a, uint64_t b, uint64_t m, uint64_t p) {
 	uint64_t ans = 0;
 	while(b) {
-		if (b & 1) ans = ans + a;
-		if (ans >= m) ans = ans - m;
-		a = a + a;
-		if (a >= m) a = a - m;
+		if (b & 1) ans = add(ans, a, m, p); 
+		a = add(a, a, m, p);
 		b = b >> 1;
 	}
 	return ans;
 }
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   a = quick_mod(a, m);
-  return ksc(a, b, m); // buggy
+  p = quick_mod(-1ULL, m);
+  p = quick_mod(p + 1, m);
+  return ksc(a, b, m, p); // buggy
 }
