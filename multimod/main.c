14a131917cdbf7da06e2defdbc40053d877a6d44
tracer-nju <tracer@nju.edu.cn>
2020-09-29 03:20:17 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 31a5163ba94f4579abc9eaff347b751b     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  03:20:17 up  3:11,  3 users,  load average: 0.04, 0.17, 0.18

--- multimod/main.c
+++ multimod/main.c
@@ -16,5 +16,5 @@ int main() {
   test(123, 456, 789);
   test(123, 456, -1ULL);
   test(-2ULL, -2ULL, -1ULL); // should be 1
-  for (int i = 1; i <= 10000; i++) test(rand(),rand(), rand());
+  for (int i = 1; i <= 10000000; i++) test(rand(),rand(), rand());
 }
