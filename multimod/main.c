4093a0b731adfa36ecfb823e31d18a2c66410fe4
tracer-nju <tracer@nju.edu.cn>
2020-09-29 03:12:59 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 31a5163ba94f4579abc9eaff347b751b     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  03:12:59 up  3:04,  3 users,  load average: 0.37, 0.31, 0.22

--- multimod/main.c
+++ multimod/main.c
@@ -8,7 +8,7 @@ uint64_t multimod(uint64_t, uint64_t, uint64_t);
 void test(uint64_t a, uint64_t b, uint64_t m) {
   #define U64 "%" PRIu64
   printf(U64 " * " U64 " mod " U64 " = " U64 "\n", a, b, m, multimod(a, b, m));
-  if (miltimod(a, b, m) != a * b % m)assert(0);
+  if (multimod(a, b, m) != a * b % m)assert(0);
 }
 
 int main() {
