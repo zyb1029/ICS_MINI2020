b39e8c7079ac0a1c5c5b61e0d7e5b20c9164af4f
tracer-nju <tracer@nju.edu.cn>
2020-10-21 10:31:38 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 22786e3baf6449b5a1d247fdf202ec46     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:31:38 up 14:47,  3 users,  load average: 0.28, 0.32, 0.26

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -5,12 +5,11 @@ int64_t asm_add(int64_t a, int64_t b) {
   asm("movq %0, %%rax;\n"
 	  "movq %1, %%rcx;\n"
 	  "addq %%rcx, %%rax;\n"
-	  "leave"
-	  "ret"
 	  :
 	  : "r"(a), "r"(b)
 	  : "%rax", "%rcx"
 	  );
+	  return 0;
 }
 
 int asm_popcnt(uint64_t x) {
