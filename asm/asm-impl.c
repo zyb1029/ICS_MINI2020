85af16c0d5a2ceadd0926b9d4c3ca6640b420d5d
tracer-nju <tracer@nju.edu.cn>
2020-10-26 13:08:43 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: be08b31627054dc98a77168ac252fb7a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:08:43 up  4:54,  3 users,  load average: 0.46, 0.61, 0.71

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -16,7 +16,7 @@ int asm_popcnt(uint64_t x) {
   for (int i = 0; i < 64; i++) {
      asm volatile("mov 1, %%rax\n\t"
 		"and %0, %%rax\n\t"
-		"shrq 1, %1\n\t"
+		"shrq 1, %2\n\t"
 		"add %%eax, %2"
 		:"=r"(x)
 	    :"r"(x), "r"(s) 
