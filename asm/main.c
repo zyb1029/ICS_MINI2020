43a9f83e81e80a3dc69b44b0919778e9f59c0b25
tracer-nju <tracer@nju.edu.cn>
2020-10-28 05:35:14 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: e257cba8b5a54c828a6f8233a1c5e07f     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  05:35:14 up  8:54,  8 users,  load average: 0.10, 0.22, 0.21

--- asm/main.c
+++ asm/main.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 int main() {
   asm_jmp_buf buf;
   int r = asm_setjmp(buf);
@@ -11,7 +12,7 @@ int main() {
     // TODO: add more tests here.
 	int a[100], n = 15, b[100];
 	for (int i = 0; i < n; i++) a[i] = rand();
-	assert(memcpy(a, asm_memcpy(b, a, n * sizeof(int)), n * sizeof(int)));
+	assert(memcmp(a, asm_memcpy(b, a, n * sizeof(int)), n * sizeof(int)));
 	for (int i = 0; i < n; i++) assert(a[i] == b[i]);
     asm_longjmp(buf, 123);
   } else {
