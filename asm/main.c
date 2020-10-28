d7733ee29acd3d80ec08843ea4d2fccfae05f269
tracer-nju <tracer@nju.edu.cn>
2020-10-28 05:30:03 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: e257cba8b5a54c828a6f8233a1c5e07f     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  05:30:03 up  8:49,  8 users,  load average: 0.31, 0.30, 0.22

--- asm/main.c
+++ asm/main.c
@@ -9,7 +9,10 @@ int main() {
     assert(asm_add(1234, 5678) == 6912);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
     // TODO: add more tests here.
-	char *s = "sdadad";
+	int a[100], n = 15, b[100];
+	for (int i = 0; i < n; i++) a[i] = rand();
+	memcpy(b, a, n * sizeof(int));
+	for (int i = 0; i < n; i++) assert(a[i] == b[i]);
     asm_longjmp(buf, 123);
   } else {
     assert(r == 123);
