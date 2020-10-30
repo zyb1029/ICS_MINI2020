5912a63612628d0facac4cae9165884203c7df34
tracer-nju <tracer@nju.edu.cn>
2020-10-30 12:33:26 -0400

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: dc772b3c1af84de0a9d3aed9d502536a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:33:27 up 43 min,  2 users,  load average: 0.08, 0.05, 0.06

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -33,9 +33,14 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
 }
 
 int asm_setjmp(asm_jmp_buf env) {
+  asm volatile("mov %[env], %%rcx;\n\t"
+               "mov [**esp], %%edx\n\t"
+			   :[env] "m"(env)
+			   );
+              
   return setjmp(env);
 }
 
 void asm_longjmp(asm_jmp_buf env, int val) {
-  longjmp(env, val);
+ // longjmp(env, val);
 }
--- asm/asm.h
+++ asm/asm.h
@@ -2,8 +2,8 @@
 #include <stddef.h>
 
 // TODO: replace with your definition
-#include <setjmp.h> // remove this
-#define asm_jmp_buf jmp_buf
+//#include <setjmp.h> // remove this
+#define asm_jmp_buf long*
 
 int64_t asm_add(int64_t a, int64_t b);
 int     asm_popcnt(uint64_t x);
