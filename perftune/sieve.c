4b179a7a763a8325948d88c5a62e7a4722f1d8fb
tracer-nju <tracer@nju.edu.cn>
2020-11-27 05:34:04 -0500

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8f1e420ff95b41f997575732a00f7acc     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  05:34:04 up 22:29, 18 users,  load average: 0.12, 0.19, 0.09

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -14,16 +14,12 @@ int *sieve(int n) {
     is_prime[i] = true;
 
   for (int i = 2; i <= n; i++) {
-    for (int j = i + i; j <= n; j += i) {
-      is_prime[j] = false;
-    }
+	  if (is_prime[i] == true) primes[tot++] = i;
+	  for (int j = 0; j < tot && i * primes[j] <= n; j++) {
+		  is_prime[i * primes[j]] = false;
+		  if (i % primes[j] == 0) break;
+	  }	  
   }
-
-  int *p = primes;
-  for (int i = 2; i <= n; i++)
-    if (is_prime[i]) {
-      *p++ = i;
-    }
-  *p = 0;
+  primes[tot] = 0;
   return primes;
 }
