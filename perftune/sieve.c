d4c3349d9a35e1c9f1cf624a3f4fd21a22370dc3
tracer-nju <tracer@nju.edu.cn>
2020-11-27 07:40:11 -0500

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8f1e420ff95b41f997575732a00f7acc     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  07:40:12 up 1 day, 35 min, 18 users,  load average: 0.08, 0.26, 0.25

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -22,18 +22,13 @@ int *sieve(int n) {
 		  k = i * primes[j + 1];
 	  }	  
 	  if (not_prime[i + 2] == false) primes[tot++] = i + 2;
+	  if (not_prime[i + 3] == false) primes[tot++] = i + 3;
 	  k = (i + 2) * primes[0];
 	  for (int j = 0; j < tot && k <= n; j++) {
 		  not_prime[k] = true;
-		  if ((i + 2) % primes[j] == 0) break;
-		  k = (i + 2) * primes[j + 1];
-	  }
-	  if (not_prime[i + 3] == false) primes[tot++] = i + 3;
-	  k = (i + 3) * primes[0];
-	  for (int j = 0; j < tot && k <= n; j++) {
-		  not_prime[k] = true;
+		  not_prime[k + primes[j]] = true;
 		  if ((i + 3) % primes[j] == 0) break;
-		  k = (i + 3) * primes[j + 1];
+		  k = (i + 2) * primes[j + 1];
 	  }
   }
   if (primes[tot - 1] >= n) tot--;
