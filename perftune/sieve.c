f16d222a6fc70b63b72945a945c6041a90567a7d
tracer-nju <tracer@nju.edu.cn>
2020-11-27 08:43:05 -0500

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8f1e420ff95b41f997575732a00f7acc     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:43:05 up 1 day,  1:38, 17 users,  load average: 0.41, 0.17, 0.12

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -9,14 +9,16 @@ static bool not_prime[N];
 static int primes[N];
 
 static int tot = 0;
-
+static int k = 0;
 int *sieve(int n) {
   assert(n + 1 < N);
   
   for (int i = 2; i <= n; i++) {
 	if (not_prime[i] == false) {
 		primes[tot++] = i;
-		for (int j = i + i; j <= n; j += i << 1)
+		if (i > 3) k = i << 1;
+		else k = i;
+		for (int j = i + i; j <= n; j += k)
 			not_prime[j] = true;
 	}
   }
