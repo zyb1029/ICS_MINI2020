8660c7ae6d5ea3c968390ed45c26a66b038e6999
tracer-nju <tracer@nju.edu.cn>
2020-11-27 09:46:14 -0500

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8f1e420ff95b41f997575732a00f7acc     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  09:46:15 up 1 day,  2:40, 17 users,  load average: 0.15, 0.18, 0.28

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define N 10000000
-#define M 10000000
+#define N 10000020
 
 static bool not_prime[N];
 static int primes[M];
@@ -13,36 +12,38 @@ static int k = 0;
 
 int *sieve(int n) {
   assert(n + 1 < N);
-  for (register int i = 2; i <= n; i += 6) {
+  for (int i = 2; i <= n; i += 6) {
 	  not_prime[i] = true;
 	  not_prime[i + 2] = true;
   }
 
-  for (register int i = 3; i <= n; i += 6)
+  for (int i = 3; i <= n; i += 6)
       not_prime[i] = true;
 
   if (n >= 2) primes[tot++] = 2;
   if (n >= 3) primes[tot++] = 3;
 
-  for (register int i = 5; i <= n; i = i + 4) {
+  for (int i = 5; i <= n; i = i + 4) {
 	if (not_prime[i] == false) {
 		primes[tot++] = i;
-		k = i * 6;
-		for (register int j = i; j <= n; j += k) {
+		k = i * 12;
+		for (int j = i; j <= n; j += k) {
 			not_prime[j] = true;
+			not_prime[j + 6] = true;
 		}
-		for (register int j = 5 * i; j <= n; j += k) {
+		for (int j = 5 * i; j <= n; j += k) {
 			not_prime[j] = true;
+			not_prime[j + 6] = true;
 		}
 	}
 	i = i + 2;
 	if (not_prime[i] == false) {
 		k = i * 6;
 		primes[tot++] = i;
-		for (register int j = i; j <= n; j += k) {
+		for (int j = i; j <= n; j += k) {
 			not_prime[j] = true;
 		}
-		for (register int j = 5 * i; j <= n; j += k) {
+		for (int j = 5 * i; j <= n; j += k) {
 			not_prime[j] = true;
 		}
 	}
