a5b2b7e810b7689f9b18f19554405857292ec547
tracer-nju <tracer@nju.edu.cn>
2020-11-27 08:28:48 -0500

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8f1e420ff95b41f997575732a00f7acc     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:28:49 up 1 day,  1:23, 17 users,  load average: 0.16, 0.12, 0.16

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -1,50 +1,25 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
 
 #define N 10000000
-#define M 1000000
 
 static bool not_prime[N];
-static int primes[M];
+static int primes[N];
 
 static int tot = 0;
-static int k = 0;
-int *sieve(int n) {
 
-  for (int i = 2; i <= n; i += 6) {
-	  if (not_prime[i] == false) primes[tot++] = i;
-	  if (not_prime[i + 1] == false) primes[tot++] = i + 1;
-	  k = i * primes[0];
-	  for (int j = 0; j < tot && k <= n; j++) {
-		  not_prime[k] = true;
-		  not_prime[k + primes[j]] = true;
-		  if ((i + 1) % primes[j] == 0) break;
-		  k = i * primes[j + 1];
-	  }
-	  if (not_prime[i + 2] == false) primes[tot++] = i + 2;
-	  if (not_prime[i + 3] == false) primes[tot++] = i + 3;
-	  k = (i + 2) * primes[0];
-	  for (int j = 0; j < tot && k <= n; j++) {
-		  not_prime[k] = true;
-		  not_prime[k + primes[j]] = true;
-		  if ((i + 3) % primes[j] == 0) break;
-		  k = (i + 2) * primes[j + 1];
-	  }
-	  if (not_prime[i + 4] == false) primes[tot++] = i + 4;
-	  if (not_prime[i + 5] == false) primes[tot++] = i + 5;
-	  k = (i + 4) * primes[0];
-	  for (int j = 0; j < tot && k <= n; j++) {
-		  not_prime[k] = true;
-		  not_prime[k + primes[j]] = true;
-		  if ((i + 5) % primes[j] == 0) break;
-		  k = (i + 4) * primes[j + 1];
-	  }
+int *sieve(int n) {
+  assert(n + 1 < N);
+  
+  for (int i = 2; i <= n; i++) {
+	if (not_prime[i] == false) {
+		primes[tot++] = i;
+		for (int j = i + i; j <= n; j += i)
+			not_prime[j] = true;
+	}
   }
-  if (primes[tot - 1] >= n) tot--;
-  if (primes[tot - 1] >= n) tot--;
-  if (primes[tot - 1] >= n) tot--;
-  if (primes[tot - 1] >= n) tot--;
-  if (primes[tot - 1] >= n) tot--;
   primes[tot] = 0;
   return primes;
 }
