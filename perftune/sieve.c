d2531934f8a5952b7645ff2365f800f4a9c11311
tracer-nju <tracer@nju.edu.cn>
2020-11-27 06:43:03 -0500

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8f1e420ff95b41f997575732a00f7acc     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  06:43:04 up 23:38, 17 users,  load average: 0.39, 0.25, 0.21

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -9,17 +9,25 @@ static bool not_prime[N];
 static int  primes[N];
 static int tot = 0;
 static int k = 0;
+static int p = 0;
 int *sieve(int n) {
   assert(n + 1 < N);
 
-  for (int i = 2; i <= n; i++) {
+  for (int i = 2; i <= n; i+= 2) {
 	  if (not_prime[i] == false) primes[tot++] = i;
+	  if (not_prime[i + 1] == false && i != n) primes[tot++] = i + 1;
 	  k = i * primes[0];
 	  for (int j = 0; j < tot && k <= n; j++) {
 		  not_prime[k] = true;
 		  if (i % primes[j] == 0) break;
 		  k = i * primes[j + 1];
 	  }	  
+	  p = (i + 1) * primes[0];
+	  for (int l = 0; l < tot && p <= n; l++) {
+		  not_prime[p] = true;
+		  if ((i + 1) % primes[l] == 0) break;
+		  p = (i + 1) * primes[l + 1];
+	  }
   }
   primes[tot] = 0;
   return primes;
